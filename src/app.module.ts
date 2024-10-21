22ea8a0d031976ebfb81cc8fc66549cfe1a36157
Sirah <hadjasira5.diallo@gmail.com>
2024-10-21 20:27:50 +0000

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
